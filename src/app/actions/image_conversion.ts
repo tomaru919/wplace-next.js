d3ecea355417e938042d0fa3a1c0d134e752dcb0
tomaru919 <kyukurarin919@gmail.com>
2026-08-30 18:23:39 +0900

    デバッグ: Bunのメインスレッドをログに出力するように変更

--- src/app/actions/image_conversion.ts
+++ src/app/actions/image_conversion.ts
@@ -140,6 +140,8 @@ export default async function imageConversion(formData: FormData) {
   const isDither = formData.get("isDither") === "true"
   const isNoPixelate = formData.get("isNoPixelate") === "true"
 
+  console.log("bun main thread", Bun.isMainThread)
+
   const imageBuffer = Buffer.from(await imageFile.arrayBuffer())
   const image = sharp(imageBuffer)
   const metadata = await image.metadata()
